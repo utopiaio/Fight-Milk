0f06f3ea7a138d1ed1e981c498bf81f9e92dc909
Moe Szyslak <moe.duffdude@gmail.com>
2016-12-14 10:14:42 +0300

    single error handler
    - all query selectors now live inside a single object
    
    TODO:
    multiple instances are "locking" up --- ANC it

--- insa.js
+++ insa.js
@@ -9,29 +9,67 @@ require('nightmare-upload')(Nightmare);
 const insa = (url, options) => {
   let insaWait = null; // this will be used to refill the form
   let cleanSweep = null; // if site freezes, restarts form processing
+
   const nightmare = Nightmare(Object.assign({
     show: true,
     typeInterval: 32,
   }, options));
 
+  const selector = {
+    // eslint-disable-next-line
+    captcha: `[style="color:white;font-weight:bold;padding-left:25px;font-size:18px;background:url('/PartnerRegistrationForm-portlet/images/capchaback.png') no-repeat;"]`,
+    name: '[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_name]',
+    tel: '[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_telno]',
+    email: '[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_email]',
+    fax: '[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_fax]',
+    tinno: '[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_tinno]',
+    attachfile: '[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_attachfile]',
+    exe: '[name=exe]',
+    usercode: '[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_usercode]',
+    submit: 'button[type=submit]',
+  };
+
+  const errorHandler = (error) => {
+    clearInterval(insaWait);
+    clearTimeout(cleanSweep);
+    nightmare.halt();
+    console.error(error);
+  };
+
   const run = (subscribe) => {
     nightmare
-      // eslint-disable-next-line
-      .evaluate(() => document.querySelector(`[style="color:white;font-weight:bold;padding-left:25px;font-size:18px;background:url('/PartnerRegistrationForm-portlet/images/capchaback.png') no-repeat;"]`).innerHTML)
+      .wait(selector.captcha)
+      .evaluate(() => {
+        let interval = null;
+
+        return new Promise((resolve) => {
+          interval = setInterval(() => {
+            if (document.querySelector(selector.captcha) !== null) {
+              clearInterval(interval);
+              resolve(document.querySelector(selector.captcha).innerHTML);
+            }
+          }, 500);
+        });
+      })
       .then((token) => {
-        // eslint-disable-next-line
-        const [name, tel, email, fax, tinno] = [faker.name.findName(), faker.phone.phoneNumber().replace(/ x.+$/, ''), faker.internet.email(), faker.phone.phoneNumber().replace(/ x.+$/, ''), faker.phone.phoneNumber().replace(/ x.+$/, '')];
+        const [name, telno, email, fax, tinno] = [
+          faker.name.findName(),
+          faker.phone.phoneNumber().replace(/ x.+$/, ''),
+          faker.internet.email(),
+          faker.phone.phoneNumber().replace(/ x.+$/, ''),
+          faker.phone.phoneNumber().replace(/ x.+$/, ''),
+        ];
 
         nightmare
-          .type('[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_name]', name)
-          .type('[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_telno]', tel)
-          .type('[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_email]', email)
-          .type('[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_fax]', fax)
-          .type('[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_tinno]', tinno)
-          .upload('[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_attachfile]', path.resolve('./🙈.pdf')) // 20MB is the max allowed
-          // .type('[name=exe]', 'pdf') [filled via js on the page - this is their MIME check]
-          .type('[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_usercode]', token)
-          .click('button[type=submit]')
+          .type(selector.nameValue, name)
+          .type(selector.tinno, telno)
+          .type(selector.email, email)
+          .type(selector.fax, fax)
+          .type(selector.tinno, tinno)
+          .upload(selector.attachfile, path.resolve('./🙈.pdf')) // 20MB is the max allowed
+          // .type(selector.exe, 'pdf') [filled via js on the page - this is their MIME check]
+          .type(selector.usercode, token)
+          .click(selector.submit)
           .then(() => {
             if (insaWait !== null) {
               clearInterval(insaWait);
@@ -40,14 +78,26 @@ const insa = (url, options) => {
 
             insaWait = setInterval(() => {
               nightmare
-                .wait('[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_name]')
-                .evaluate(() => document.querySelector('[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_name]').value)
+                .wait(selector.name)
+                .evaluate(() => {
+                  let interval = null;
+
+                  return new Promise((resolve) => {
+                    interval = setInterval(() => {
+                      if (document.querySelector(selector.name) !== null) {
+                        clearInterval(interval);
+                        resolve(document.querySelector(selector.name).value);
+                      }
+                    }, 500);
+                  });
+                })
                 .then((nameValue) => {
                   // form has been submitted and waiting for form reset...
                   if (typeof nameValue === 'string' && nameValue.length === 0) {
                     if (subscribe) {
-                      subscribe({ name, tel, email, fax, tinno });
+                      subscribe({ name, telno, email, fax, tinno });
                     }
+
                     clearInterval(insaWait);
                     clearTimeout(cleanSweep);
                     run(subscribe);
@@ -59,36 +109,46 @@ const insa = (url, options) => {
             cleanSweep = setTimeout(() => {
               nightmare
                 .evaluate(() => {
-                  console.log('resetting form...');
-                  document.querySelector('[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_name]').value = '';
-                  document.querySelector('[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_telno]').value = '';
-                  document.querySelector('[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_email]').value = '';
-                  document.querySelector('[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_fax]').value = '';
-                  document.querySelector('[name=_partneractionclass_WAR_PartnerRegistrationFormportlet_tinno]').value = '';
-                  return 'cleared';
+                  let interval = null;
+
+                  return new Promise((resolve) => {
+                    interval = setInterval(() => {
+                      if (
+                        document.querySelector(selector.name) !== null &&
+                        document.querySelector(selector.telno) !== null &&
+                        document.querySelector(selector.email) !== null &&
+                        document.querySelector(selector.fax) !== null &&
+                        document.querySelector(selector.tinno) !== null
+                      ) {
+                        clearInterval(interval);
+                        document.querySelector(selector.name).value = '';
+                        document.querySelector(selector.telno).value = '';
+                        document.querySelector(selector.email).value = '';
+                        document.querySelector(selector.fax).value = '';
+                        document.querySelector(selector.tinno).value = '';
+                        resolve();
+                      }
+                    }, 500);
+                  });
                 })
                 .then(() => {
+                  console.log('> form cleared');
                   clearInterval(insaWait);
+                  clearTimeout(cleanSweep);
                   run(subscribe);
-                });
+                })
+                .catch(errorHandler);
             }, 5000);
           })
-          .catch((error) => {
-            nightmare.end();
-            console.error(error);
-          });
+          .catch(errorHandler);
       })
-      .catch((err) => {
-        nightmare.end();
-        console.error(err);
-      });
+      .catch(errorHandler);
   };
 
   try {
     nightmare.goto(url);
-  } catch (err) {
-    nightmare.end();
-    console.error(err);
+  } catch (error) {
+    errorHandler(error);
   }
 
   return {
